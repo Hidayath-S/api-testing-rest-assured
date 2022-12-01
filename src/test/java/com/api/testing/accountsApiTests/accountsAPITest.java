2387fdf6a2b448d8e36438660baa81fd8635ccdf
Hidayath-S <hiddu91@gmail.com>
2022-12-01 20:48:47 +0530

    Adding parameterised test

--- src/test/java/com/api/testing/accountsApiTests/accountsAPITest.java
+++ src/test/java/com/api/testing/accountsApiTests/accountsAPITest.java
@@ -1,13 +1,11 @@
 package com.api.testing.accountsApiTests;
 
-import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import java.io.File;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,32 +16,39 @@ public class accountsAPITest {
     private static final String URI = "http://localhost:8114";
 
     @BeforeAll
-    public static void setUp(){
+    public static void setUp() {
         baseURI = URI;
 
     }
 
     /**
      * getAccountByAcctNum API requires a request param accountNum
+     * Using @CSVSource to parameteraize the test
      */
-    @Test
-    public void getAccountByAcctNumTest(){
-        Response response = given().get("Account?accountNum=12356887888");
-        assertEquals(200,response.getStatusCode());
+    @ParameterizedTest
+    @CsvSource(value = {"1235688755,Active,1000,custId1", "1235688756,Active,2000,custId1",
+            "1235688757,Active,3000,custId2", "1235688758,Active,3000,custId2"})
+    public void getAccountByAcctNumTest(Integer accountNum, String acctSts, Float balance, String custId) {
+        Response response = given().request().log().all()
+                .queryParam("accountNum",accountNum)
+                .get("/Account");
+        //System.out.println(response.prettyPrint());
+        assertEquals(200, response.getStatusCode());
 
         JsonPath jsonPath = response.jsonPath();
 
         // asserting values from the response using jsonPath
-        assertEquals(Long.valueOf("12356887888"),jsonPath.get("accountNumber"));
-        assertEquals("custId1",jsonPath.get("custId"));
-        assertEquals("Active",jsonPath.get("acctSts"));
+        assertEquals(accountNum, jsonPath.get("accountNumber"));
+        assertEquals(balance, jsonPath.get("balance"));
+        assertEquals(custId, jsonPath.get("custId"));
+        assertEquals(acctSts,jsonPath.get("acctSts"));
 
         //asserting using hamcrest matchers
-        assertThat(jsonPath.get("balance"), Matchers.notNullValue());
+        assertThat(jsonPath.get("balance"), Matchers.is(instanceOf(Float.class)));
+        assertThat(jsonPath.get("accountNumber"),Matchers.is(instanceOf(Integer.class)));
+        assertThat(jsonPath.get("acctSts"),Matchers.is(instanceOf(String.class)));
+        assertThat(jsonPath.get("custId"),Matchers.is(instanceOf(String.class)));
 
-        //validating json schema
-        given().when().get("Account?accountNum=12356887888")
-                .then().assertThat().statusCode(200).body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/resources/jsonSchema/getAccountByAcctNumSchema.json")));
     }
 
 }
